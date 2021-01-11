881750fe9b426924c2202b04d210b3d6656dc54e
marc1 <m@marc.rs>
2021-01-11 11:33:25 -0800

    ?

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -15,7 +15,7 @@ ln -s $WORKSPACE/cfg $CFG
 #################################
 
 ######## Install nix and brew ########
-/bin/bash -c "$(<(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume)"
+sh <$(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume
 nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
 ./result/bin/darwin-installer
 rm -r ./result
